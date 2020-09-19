416eb98a6016d6f9e6c6d98c07dfede2b0da25d7
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2020-09-19 15:52:04 +0300

    Intest.sh
    Umm, my first script..? (Just made in nano with Termux)

---
+++ Intest.sh
@@ -0,0 +1,17 @@
+clear
+echo ======================
+echo IP
+echo ======================
+curl 2ip.ua
+echo ======================
+echo Ping to 8.8.8.8
+echo ======================
+ping -c 5 8.8.8.8
+wait 5
+exit
+## Its just testing internet timeout and your ip.
+## Nothing is new.
+
+## EXTRAS 
+## 8.8.8.8 - Google Public DNS
+## :D
